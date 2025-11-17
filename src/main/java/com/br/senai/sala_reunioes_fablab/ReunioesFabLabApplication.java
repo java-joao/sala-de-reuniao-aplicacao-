58f3abdee0cb6bff14a8a2e0ccc81e6d38b9fcbd
gabriellegomesb <gabrielle.gomes.bandeira15@gmail.com>
2025-11-17 10:30:16 -0300

    trabalho

---
+++ src/main/java/com/br/senai/sala_reunioes_fablab/ReunioesFabLabApplication.java
@@ -0,0 +1,13 @@
+package com.br.senai.sala_reunioes_fablab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReunioesFabLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReunioesFabLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/senai/sala_reunioes_fablab/ReunioesFabLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.senai.sala_reunioes_fablab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReunioesFabLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
