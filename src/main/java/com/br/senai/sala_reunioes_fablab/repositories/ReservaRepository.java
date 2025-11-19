6a92e74ca97059b0bf694cee5701a1335e86a279
Gabi Gomes <gabrielle.gomes.bandeira15@gmail.com>
2025-11-19 11:22:23 -0300

    Add ReservaRepository interface for Reserva model

--- src/main/java/com/br/senai/sala_reunioes_fablab/repositories/ReservaRepository.java
+++ src/main/java/com/br/senai/sala_reunioes_fablab/repositories/ReservaRepository.java
@@ -1,5 +1,12 @@
 package com.br.senai.sala_reunioes_fablab.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.senai.infoa.trabalhosenai.models.Reserva;
+
+
+
+
 public interface ReservaRepository extends JpaRepository <Reserva, Integer>{
     
     
