aebc4807f1687a6219a2fd7d4856df783f55635f
Gabi Gomes <gabrielle.gomes.bandeira15@gmail.com>
2025-11-19 11:23:22 -0300

    Add ResponsavelRepository interface for Responsavel model

--- src/main/java/com/br/senai/sala_reunioes_fablab/repositories/ResponsavelRepository.java
+++ src/main/java/com/br/senai/sala_reunioes_fablab/repositories/ResponsavelRepository.java
@@ -1,5 +1,13 @@
 package com.br.senai.sala_reunioes_fablab.repositories;
 
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.senai.infoa.trabalhosenai.models.Responsavel;
+
+
+
 public interface ResponsavelRepository extends JpaRepository<Responsavel, Integer>{
     
 }
