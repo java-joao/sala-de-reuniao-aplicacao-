0d3e585633a9889512b26ba79d72849ef50f996f
Gabi Gomes <gabrielle.gomes.bandeira15@gmail.com>
2025-11-19 11:36:45 -0300

    import

--- src/main/java/com/br/senai/sala_reunioes_fablab/repositories/AdministradorRepository.java
+++ src/main/java/com/br/senai/sala_reunioes_fablab/repositories/AdministradorRepository.java
@@ -3,7 +3,7 @@ package com.br.senai.sala_reunioes_fablab.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.senai.infoa.trabalhosenai.models.Administrador;
+import com.br.senai.sala_reunioes_fablab.models.Administrador;
 
 @Repository
 public interface AdministradorRepository extends JpaRepository<Administrador, Integer>{
