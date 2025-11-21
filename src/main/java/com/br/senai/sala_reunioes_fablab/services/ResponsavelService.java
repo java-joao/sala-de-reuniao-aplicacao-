964013029287a00c4e7c9541ab64c21c635c29ac
Thiagotesch7 <thiagotesch7@gmail.com>
2025-11-21 11:18:39 -0300

    Update ResponsavelService.java

--- src/main/java/com/br/senai/sala_reunioes_fablab/services/ResponsavelService.java
+++ src/main/java/com/br/senai/sala_reunioes_fablab/services/ResponsavelService.java
@@ -3,18 +3,30 @@ package com.br.senai.sala_reunioes_fablab.services;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
-
+import com.br.senai.sala_reunioes_fablab.repositories.ReservaRepository;
 import com.br.senai.sala_reunioes_fablab.repositories.ResponsavelRepository;
+import com.br.senai.sala_reunioes_fablab.models.Administrador;
 import com.br.senai.sala_reunioes_fablab.models.Responsavel;
 
 @Service
 public class ResponsavelService {
 
+    private final ReservaRepository reservaRepository;
+
     @Autowired
     private ResponsavelRepository responsavelRepository;
 
+    ResponsavelService(ReservaRepository reservaRepository) {
+        this.reservaRepository = reservaRepository;
+    }
+
     public void cadastrarResponsavel(Responsavel responsavel) {
         responsavelRepository.save(responsavel);
+    }
+
+    public Responsavel loginUsuario(String nome, String senha) {
+        return responsavelRepository.LoginEmaileSenha(nome, senha);
+            
     }
 
     public List<Responsavel> consultarResponsaveis() {
