49b62638fd659bff0777b1b430dba34a6680fb8a
Thiagotesch7 <thiagotesch7@gmail.com>
2025-11-21 11:17:36 -0300

    Update AdministradorRepository.java

--- src/main/java/com/br/senai/sala_reunioes_fablab/repositories/AdministradorRepository.java
+++ src/main/java/com/br/senai/sala_reunioes_fablab/repositories/AdministradorRepository.java
@@ -1,6 +1,8 @@
 package com.br.senai.sala_reunioes_fablab.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.br.senai.sala_reunioes_fablab.models.Administrador;
@@ -8,4 +10,6 @@ import com.br.senai.sala_reunioes_fablab.models.Administrador;
 @Repository
 public interface AdministradorRepository extends JpaRepository<Administrador, Integer>{
     
+    @Query(value = "SELECT * FROM administrador WHERE nome= :nome AND senha=:senha",nativeQuery=true)
+    public Administrador LoginEmaileSenha (@Param("nome") String nome,@Param("senha")String senha);
 }
