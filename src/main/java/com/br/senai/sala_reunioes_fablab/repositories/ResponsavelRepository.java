281867936c42db47f8a316a5a9d90bb184de9b59
Gabi Gomes <gabrielle.gomes.bandeira15@gmail.com>
2025-11-21 11:25:34 -0300

    Add custom query for login in ResponsavelRepository

--- src/main/java/com/br/senai/sala_reunioes_fablab/repositories/ResponsavelRepository.java
+++ src/main/java/com/br/senai/sala_reunioes_fablab/repositories/ResponsavelRepository.java
@@ -9,5 +9,6 @@ import com.br.senai.sala_reunioes_fablab.models.Responsavel;
 
 @Repository
 public interface ResponsavelRepository extends JpaRepository<Responsavel, Integer>{
-    
+    @Query(value = "SELECT * FROM responsavel WHERE nome= :nome AND senha=:senha",nativeQuery=true)
+    public Responsavel LoginEmaileSenha (@Param("nome") String nome,@Param("senha")String senha);
 }
