311ff63a00dfd9386039e18dbb4a07ef343e7b1b
Gabi Gomes <gabrielle.gomes.bandeira15@gmail.com>
2025-11-21 11:32:52 -0300

    Refactor ResponsavelRepository query formatting

--- src/main/java/com/br/senai/sala_reunioes_fablab/repositories/ResponsavelRepository.java
+++ src/main/java/com/br/senai/sala_reunioes_fablab/repositories/ResponsavelRepository.java
@@ -1,14 +1,21 @@
 package com.br.senai.sala_reunioes_fablab.repositories;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;     // IMPORT NECESSÁRIO
+import org.springframework.data.repository.query.Param; // IMPORT NECESSÁRIO
 
 import com.br.senai.sala_reunioes_fablab.models.Responsavel;
 
-
 @Repository
-public interface ResponsavelRepository extends JpaRepository<Responsavel, Integer>{
-    @Query(value = "SELECT * FROM responsavel WHERE nome= :nome AND senha=:senha",nativeQuery=true)
-    public Responsavel LoginEmaileSenha (@Param("nome") String nome,@Param("senha")String senha);
+public interface ResponsavelRepository extends JpaRepository<Responsavel, Integer> {
+
+    @Query(
+        value = "SELECT * FROM responsavel WHERE nome = :nome AND senha = :senha",
+        nativeQuery = true
+    )
+    public Responsavel LoginEmaileSenha(
+        @Param("nome") String nome,
+        @Param("senha") String senha
+    );
 }
